5c11aa5d2ea4873eea53d56e1e3331e73b3d15d3
Erdem Köşk <erdemkosk@gmail.com>
2023-11-26 18:55:27 +0300

    Added buffered channels with select

--- select.go
+++ select.go
@@ -5,25 +5,17 @@ import (
 	"time"
 )
 
-// The select statement is used to listen for data coming from multiple channels concurrently, but the crucial aspect is the concurrent (or asynchronous) nature.
-// Especially when multiple tasks are being performed, using the select statement allows simultaneous access to data from channels and optimizes the processing time of the program.
-
-// For instance, when waiting for data from the network or different threads of execution, it enables the main thread to continue doing other tasks while waiting for data from channels.
-//  Instead of waiting idly until data arrives from any channel, select concurrently monitors multiple channels and acts accordingly when data arrives.
-
-// The main thread waits until data is received from any of the channels being listened to with the select statement. However, it can also continue with other tasks,
-//  execute other threads of execution, or perform different operations.
-
-// This feature allows programs to monitor and manage various tasks concurrently, preventing the program from blocking in unexpected situations.
-// The use of select is highly beneficial in scenarios involving asynchronous operations or listening for data from various sources.
-
 func SelectExample() {
 	fmt.Println("--------------------------")
 	channel3 := make(chan string)
 	channel4 := make(chan string)
 
+	charChannel := make(chan string, 5)
+
 	go goRoutine3(channel3)
 	go goRoutine4(channel4)
+
+	go fillBufferedChannel(charChannel)
 
 	select {
 	case msg1 := <-channel3:
@@ -32,15 +24,34 @@ func SelectExample() {
 		fmt.Println("Received from Channel 4:", msg2)
 	}
 
+	var result string
+	for i := 0; i < 5; i++ {
+		select {
+		case char := <-charChannel:
+			result += char
+		}
+	}
+
+	fmt.Println("Received:", result)
 	fmt.Println("Hello From Select!")
 }
 
 func goRoutine3(c chan string) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(4 * time.Second)
 	c <- "3"
 }
 
 func goRoutine4(c chan string) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(4 * time.Second)
 	c <- "4"
+}
+
+func fillBufferedChannel(c chan string) {
+	chars := []string{"e", "r", "d", "e", "m"}
+
+	for _, v := range chars {
+		c <- v
+	}
+
+	close(c)
 }
